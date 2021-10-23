be34c3a42a9d6fb4b7d3695c49382e7e6a04e048
DanPGolden <mary_dangolden@comcast.net>
2021-10-23 10:05:56 -0700

    fixing ajax command

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function searchCity(cityname) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
     var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=ecc0be5fd92206da3aa90cc41c13ca56";
 
-    $.ajax({
+    $.ajax({ 
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
